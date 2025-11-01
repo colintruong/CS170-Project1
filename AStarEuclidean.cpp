4d892fa96c8b74a2cea3ac026ab2e3eaa5d7dba4
Bavly Shehata <bavlyshehata@Mac.lan>
2025-11-01 10:12:30 -0700

    Addeda a function that finds the next moves of the puzzle. I also added a simple test in main to make sure the code is still working so far

--- AStarEuclidean.cpp
+++ AStarEuclidean.cpp
@@ -69,8 +69,9 @@ struct Node{
     vector<vector<int>> state;
     int cost = 0;
     double heuristic = 0.0;
+    Node* parent;
 
-    Node(vector<vector<int>> s, int startCost, double heurCost) : state(s), cost(startCost), heuristic(heurCost) {}
+    Node(vector<vector<int>> s, int startCost, double heurCost, Node* parent = nullptr) : state(s), cost(startCost), heuristic(heurCost), parent(parent) {}
 
     double totalCost() const{
         return cost + heuristic;
@@ -82,6 +83,35 @@ struct Compare{
         return (a->totalCost() > b->totalCost());
     }
 };
+
+vector<vector<vector<int>>> nextStates(const vector<vector<int>> &state){
+    vector<vector<vector<int>>> next;
+    int x;
+    int y;
+    for(int i = 0; i < 3; ++i){
+        for(int j = 0; j < 3; ++j){
+            if(state[i][j] == 0){
+                x = i;
+                y = j;
+            }
+        }
+    }
+    int xDirection[4] = {-1,1,0,0};
+    int yDirection[4] = {0,0,-1,1};
+    for(int k = 0; k<4; ++k){
+        int nextX = x + xDirection[k];
+        int nextY = y + yDirection[k];
+
+        if(nextX >= 0 && nextX <3){
+            if(nextY>=0 && nextY <3){
+                vector<vector<int>> nextState = state;
+                swap(nextState[x][y], nextState[nextX][nextY]);
+                next.push_back(nextState);
+            }
+        }
+    }
+    return next;
+}
 
 int main(){
     cout << "A* Euclidean Heuristic 8-Puzzle Solver" << endl;
@@ -97,7 +127,10 @@ int main(){
     cout << heuristic << endl;
     Node nodeTest(state, 3, heuristic);
     cout << nodeTest.cost << " " << nodeTest.heuristic << " " << nodeTest.totalCost() << endl;
-
+    vector<vector<vector<int>>> nxt = nextStates(state);
+    for(int i = 0; i<nxt.size(); ++i){
+        printPuzzleState(nxt[i]);
+    }
     return 0;
 }
 
